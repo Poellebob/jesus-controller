83af5b2a7dfcb3e6603b8ac6cfc4cded29369a0e
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2026-01-07 14:26:10 +0100

    hehe

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,37 @@
 #include "printf.h"
 #include <SPI.h>
 
-#define CE_PIN 7
-#define CSN_PIN 8
+#define CE_PIN 9
+#define CSN_PIN 10
+
+#define up_button 2
+#define down_button 4
+#define left_button 5
+#define right_button 3
+#define start_button 6
+#define select_button 7
+#define analog_button 8
+#define x_axis A0
+#define y_axis A1
+int buttons[] = {up_button,    down_button,   left_button,  right_button,
+                 start_button, select_button, analog_button};
 
 RF24 radio(CE_PIN, CSN_PIN);
 
 uint8_t address[][6] = {"1Node", "2Node"};
-bool radioNumber = 0; // opposite of the car
+bool radioNumber = 0; // opposite of car
 bool role = true;     // controller starts as TX
-float payload = 0.0;
+int16_t payload[9];
 
 void setup() {
   Serial.begin(9600);
-  radio.begin();
+
+  if (!radio.begin()) {
+    Serial.println(F("nRF24 hardware not responding!"));
+    while (true) {
+    }
+  }
+
   radio.setPALevel(RF24_PA_LOW);
   radio.setPayloadSize(sizeof(payload));
 
@@ -24,40 +42,26 @@ void setup() {
   printf_begin();
   radio.printPrettyDetails();
 
-  radio.stopListening();
+  if (role)
+    radio.stopListening();
+  else
+    radio.startListening();
+
+  Serial.println(F("Controller ready. Press T to transmit, R to receive"));
 }
 
 void loop() {
+  payload[0] = analogRead(x_axis); // throttle
+  payload[1] = analogRead(y_axis); // steering
 
-  if (role) {
-    payload += 0.5;
-    radio.write(&payload, sizeof(float));
-    Serial.print("Sent: ");
-    Serial.println(payload);
-    delay(1000);
-  }
+  payload[2] = digitalRead(up_button);
+  payload[3] = digitalRead(down_button);
+  payload[4] = digitalRead(left_button);
+  payload[5] = digitalRead(right_button);
+  payload[6] = digitalRead(start_button);
+  payload[7] = digitalRead(select_button);
+  payload[8] = digitalRead(analog_button);
 
-  if (Serial.available()) {
-    char c = toupper(Serial.read());
-
-    if (c == 'R') {
-      role = false;
-      radio.startListening();
-      Serial.println("Controller RX mode");
-    }
-
-    if (c == 'T') {
-      role = true;
-      radio.stopListening();
-      Serial.println("Controller TX mode");
-    }
-  }
-
-  if (!role) {
-    if (radio.available()) {
-      radio.read(&payload, sizeof(payload));
-      Serial.print("Received: ");
-      Serial.println(payload);
-    }
-  }
+  radio.write(&payload, sizeof(payload));
+  delay(10);
 }
